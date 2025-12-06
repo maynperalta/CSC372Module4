fe57a02c687aa9a22046d107bb44a97fa9da7b47
Maynard Peralta <maynperalta@gmail.com>
2025-12-06 15:07:56 -0700

    Added Sphere class

--- CSC372Module4/src/main/Main.java
+++ CSC372Module4/src/main/Main.java
@@ -1,6 +1,32 @@
 package main;
 
 public class Main {
+	
+	public class Sphere extends Shape {
+		private double radius;
+		
+		public Sphere(double radius) {
+			this.radius = radius;
+		}
+		
+		@Override
+		public double surface_area() {
+			return 4 * Math.PI * Math.pow(radius, 2);
+		}
+		
+		@Override
+		public double volume() {
+			return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+		}
+		
+		@Override
+		public String toString() {
+			return "Sphere: \n" + 
+					"Radius: "+ radius + "\n" + 
+					"Surface Area: " + String.format("%.2f", surface_area()) + "\n" + 
+					"Volume: " + String.format("%.2f", volume()) + "\n";
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
