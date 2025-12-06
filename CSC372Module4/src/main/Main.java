995da7a2735018aa93830ca002fde4d86b9eabf6
Maynard Peralta <maynperalta@gmail.com>
2025-12-06 16:43:53 -0700

    cleaned code and added comments.

--- CSC372Module4/src/main/Main.java
+++ CSC372Module4/src/main/Main.java
@@ -1,26 +1,26 @@
 package main;
-
+// import ArrayList for driver class requirements
 import java.util.ArrayList;
 
 public class Main {
-	
+// Sphere class	
 	public static class Sphere extends Shape {
 		private double radius;
-		
+// Parameterized constructor		
 		public Sphere(double radius) {
 			this.radius = radius;
 		}
-		
+// Surface area		
 		@Override
 		public double surface_area() {
 			return 4 * Math.PI * Math.pow(radius, 2);
 		}
-		
+// Volume		
 		@Override
 		public double volume() {
 			return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
 		}
-		
+// toString method	
 		@Override
 		public String toString() {
 			return "Sphere: \n" + 
@@ -29,7 +29,7 @@ public class Main {
 					"Volume: " + String.format("%.2f", volume()) + "\n";
 		}
 	}
-	
+// Cylinder class	
 	public static class Cylinder extends Shape {
 		private double radius;
 		private double height;
@@ -38,7 +38,6 @@ public class Main {
 			this.radius = radius;
 			this.height = height;
 		}
-		
 		@Override
 		public double surface_area() {
 			return (2 * Math.PI * radius * height) + (2 * Math.PI * Math.pow(radius, 2));
@@ -47,7 +46,6 @@ public class Main {
 		public double volume() {
 			return (Math.PI * Math.pow(radius, 2) * height);
 		}
-		
 		@Override
 		public String toString() {
 			return "Cylinder: \n" + 
@@ -57,7 +55,7 @@ public class Main {
 					"Volume: " + String.format("%.2f", volume()) + "\n";
 		}
 	}
-	
+// Cone class
 	public static class Cone extends Shape {
 		private double radius;
 		private double height;
@@ -65,18 +63,15 @@ public class Main {
 		public Cone(double radius, double height) {
 			this.radius = radius;
 			this.height = height;
-		}
-		
+		}	
 		@Override
 		public double surface_area() {
 			return Math.PI * radius * (radius + Math.sqrt(Math.pow(radius, 2) + Math.pow(height, 2)));
 		}
-		
 		@Override
 		public double volume() {
 			return Math.PI * Math.pow(radius, 2) * (height / 3);
 		}
-		
 		@Override
 		public String toString() {
 			return "Cone: \n" + 
@@ -86,8 +81,7 @@ public class Main {
 					"Volume: " + String.format("%.2f", volume()) + "\n";
 		}
 	}
-	
-	
+// Driver class to instantiate each shape and save to array	
 	public static class ShapeArray {
 		public static void main(String[] args) {
 			
@@ -99,11 +93,10 @@ public class Main {
 			shapeArray.add(cylinder);
 			Shape cone = new Cone(8.5, 4.0);
 			shapeArray.add(cone);
-			
+// Loop through array and print using toString method			
 			for (int i = 0; i < shapeArray.size(); i ++) {
 				System.out.println(shapeArray.get(i));
 			}
-
 		}
 	}
 }
